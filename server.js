5f12e6460721467c05fa56b740366af294a933f1
yourname <acrab830217@gmail.comit config --global user.name acrab830217git config --global user.email acrab830217@gmail.com>
2025-12-04 15:42:13 +0800

    排錯中

--- server.js
+++ server.js
@@ -232,12 +232,6 @@ app.get("/api/health", (req, res) => {
 // 通用：依城市取得天氣，例如 /api/weather?city=高雄市
 app.get("/api/weather", getWeatherByCity);
 
-// 如果你想保留舊的 /kaohsiung 路徑，也可以這樣接回來：
-app.get("/api/weather/kaohsiung", (req, res, next) => {
-  req.query.city = "高雄市";
-  getWeatherByCity(req, res, next);
-});
-
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
